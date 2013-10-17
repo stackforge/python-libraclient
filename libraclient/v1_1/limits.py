209fb0401c17743c92fca1d4c06388e654d5e4bd
Endre Karlson <endre.karlson@gmail.com>
2013-10-17 13:37:50 +0200

    Switch client code towards OSLO
    * Uses auth plugins - is still WIP but using a working one for now.
      Will have to re-visit this once it lands in keystone / oslo update.
    * Resource and Manager abstractions.
    * Python 3 compatability.
    * Split the API/Bindings and Shell code
    * Use entrypoints for versions
    * Mandatory options are now positional
      Example: libra <cmd> --id <id> is now libra <cmd> <id>
    
    Change-Id: I42275cc88be5689f040864e195e48c5ebacb2cea

---
+++ libraclient/v1_1/limits.py
@@ -0,0 +1,19 @@
+from libraclient.openstack.common.apiclient import base
+
+
+class Limit(base.Resource):
+    def __repr__(self):
+        return '<Limit: %s>' % self.name
+
+
+class LimitManager(base.BaseManager):
+    resource_class = Limit
+
+    def list_limits(self):
+        limits = []
+        json = self.client.get('/limits').json()
+        for lname, lvalues in json['limits'].items():
+            values = lvalues['values']
+            values['name'] = lname
+            limits.append(Limit(self, values))
+        return limits
