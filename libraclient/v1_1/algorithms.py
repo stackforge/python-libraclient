f1b9fbbd5447ba933e33434761ecee8f615e9d9a
Endre Karlson <endre.karlson@gmail.com>
2013-10-17 13:37:50 +0200

    Switch client code towards OSLO
    * Uses auth plugins - is still WIP but using a working one for now.
    * Resource and Manager abstractions.
    * Python 3 compatability.
    * Split the API/Bindings and Shell code
    * Use entrypoints for versions
    
    Change-Id: I42275cc88be5689f040864e195e48c5ebacb2cea

---
+++ libraclient/v1_1/algorithms.py
@@ -0,0 +1,15 @@
+from libraclient.openstack.common.apiclient import base
+from libraclient.v1_1.base import Manager
+
+
+class Algorithm(base.Resource):
+    def __repr__(self):
+        return '<Algorithm: %s>' % self.name
+
+
+
+class AlgorithmManager(Manager):
+    resource_class = Algorithm
+
+    def list(self):
+        return self._list('/algorithms', 'algorithms')
