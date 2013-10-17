123df557862d240cc86d90e86f895565fcd90c13
Endre Karlson <endre.karlson@gmail.com>
2013-10-17 13:37:50 +0200

    Switch client code towards OSLO
    * Uses auth plugins - is still WIP but using a working one for now.
    * Resource and Manager abstractions.
    * Python 3 compatability.
    * Split the API/Bindings and Shell code
    * Use entrypoints for versions
    
    Change-Id: I42275cc88be5689f040864e195e48c5ebacb2cea

---
+++ libraclient/v1_1/shell.py
@@ -0,0 +1,171 @@
+import argparse
+from libraclient.openstack.common import cliutils
+from libraclient import utils
+
+
+def _format_node(node):
+    return '{status}/{condition} - {id} - {address}:{port}'.format(**node)
+
+
+@cliutils.arg(
+    '--name',
+    type=str,
+    help='Name of the new LoadBalancer.',
+    required=True)
+@cliutils.arg(
+    '--port',
+    help='port for the load balancer, 80 is default')
+@cliutils.arg(
+    '--protocol',
+    help='protocol for the load balancer, HTTP is default',
+    choices=['HTTP', 'TCP', 'GALERA'])
+@cliutils.arg(
+    '--algorithm',
+    help='algorithm for the load balancer,'
+        ' ROUND_ROBIN is default',
+    choices=['LEAST_CONNECTIONS', 'ROUND_ROBIN'])
+@cliutils.arg(
+    '--node',
+    help='a node for the load balancer in ip:port format',
+    action='append', required=True)
+@cliutils.arg(
+    '--vip',
+    help='the virtual IP to attach the load balancer to')
+def do_create(cs, args):
+    data = {}
+    data['name'] = args.name
+
+    lb = cs.loadbalancers.create(
+        name=args.name,
+        port=args.port,
+        protocol=args.protocol,
+        algorithm=args.algorithm,
+        nodes=args.node,
+        virtual_ips=args.vip)
+    cliutils.print_dict(lb._info)
+
+
+@cliutils.arg(
+    'id',
+    type=str,
+    help='ID to get')
+def do_show(cs, args):
+    lb = cs.loadbalancers.get(args.id)
+
+    info = {}
+    info.update(lb._info)
+    info['nodes'] = "\n".join(map(cs.loadbalancers.format_node, lb._info['nodes']))
+
+    cliutils.print_dict(info, 'Load Balancer')
+
+
+@cliutils.arg(
+    '--deleted',
+    default=False,
+    action='store_true',
+    help='Display deleted LBs.')
+def do_list(cs, args):
+    lbs = cs.loadbalancers.list(deleted=args.deleted)
+    fields = [
+        ('id', 'ID'),
+        'name',
+        'protocol',
+        'port',
+        'status',
+        'algorithm',
+        'created',
+        'updated',
+        ('nodeCount', 'Node Count')]
+    utils.print_list(lbs, fields=fields, titled=True)
+
+
+@cliutils.arg('id', help='load balancer ID')
+@cliutils.arg('--name', help='new name for the load balancer')
+@cliutils.arg('--algorithm',
+                help='new algorithm for the load balancer',
+                choices=['LEAST_CONNECTIONS', 'ROUND_ROBIN'])
+def do_update(cs, args):
+    cs.loadbalancers.update(
+        args.id,
+        name=args.name,
+        algorithm=args.algorithm)
+
+do_modify = do_update
+
+
+@cliutils.arg(
+    'id',
+    type=int,
+    help='ID to delete')
+def do_delete(cs, args):
+    cs.loadbalancers.delete(args.id)
+
+
+@cliutils.arg(
+    'lb_id',
+    type=str,
+    help='ID of the LoadBalancer the Nodes belongs to.')
+def do_node_list(cs, args):
+    """
+    List LoadBalancer Nodes.
+    """
+    nodes = cs.loadbalancers.list_nodes(args.lb_id)
+    fields = ["id", "address", "port", "condition", "status"]
+    utils.print_list(nodes, fields=fields, titled=True)
+
+
+@cliutils.arg(
+    'lb_id',
+    type=str,
+    help='ID of the LoadBalancer that the Node belongs to.')
+@cliutils.arg(
+    'node_id',
+    type=str,
+    help='ID of the Node to show.')
+def do_node_show(cs, args):
+    """
+    Show a Node belonging to a LoadBalancer.
+    """
+    node = cs.loadbalancers.get_node(args.lb_id, args.node_id)
+    cliutils.print_dict(node._info)
+
+
+@cliutils.arg(
+    'id',
+    type=str,
+    help='ID to show Virtual IPs for.')
+def do_virtualips(cs, args):
+    vips = cs.loadbalancers.list_vip(args.id)
+    fields = [
+        'id',
+        'type',
+        ('ipVersion', 'IP Version'),
+        'address'
+    ]
+    utils.print_list(vips, fields=fields, titled=True)
+
+
+# Non LB specific commands
+def do_algorithm_list(cs, args):
+    algs = cs.algorithms.list()
+    fields = [('name', 'Algorithm Name')]
+    utils.print_list(algs, fields=fields)
+
+
+# TODO: Figure out the printing of this one
+def do_limit_list(cs, args):
+    limits = cs.limits.list_limits()
+    out = []
+    for l in limits:
+        info = l._info
+        del info['name']
+        info = "\n".join(['%s: %s' % (k, info[k])
+                         for k in sorted(info.keys())])
+        out.append({'name': l.name, 'info': info})
+    fields = ['name', 'info']
+    utils.print_list(out, fields=fields)
+
+
+def do_protocol_list(cs, args):
+    protocols = cs.protocols.list()
+    utils.print_list(protocols, titled=True)
